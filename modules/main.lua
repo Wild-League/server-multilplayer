306483bde857d0f8d63a6e975443da16b02d6197
rodrigomaganha <rodrigomaganha@fetch.ly>
2024-04-20 00:36:26 -0300

    feat: initial commit

---
+++ modules/main.lua
@@ -0,0 +1,13 @@
+local nk = require("nakama")
+
+local function matchmaker_matched(context, matched_users)
+    local matched_users_ids = {}
+
+    for _, user in pairs(matched_users) do
+        table.insert(matched_users_ids, user.presence.user_id)
+    end
+
+    return nk.match_create("match", { presences = matched_users_ids })
+end
+
+nk.register_matchmaker_matched(matchmaker_matched)
